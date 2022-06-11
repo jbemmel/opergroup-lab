41342a34eee95f8c0a1671607637ea5ca01a9f1a
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-06-11 18:18:40 -0500

    Reinvoked logic works

--- lldp_before_l2cp.py
+++ lldp_before_l2cp.py
@@ -8,7 +8,7 @@ def event_handler_main(in_json_str):
     paths = in_json["paths"]
     options = in_json["options"]
 
-    lldp = "persistent-data" in in_json
+    reinvoked = "persistent-data" in in_json and in_json["persistent-data"]["reinvoked"]
 
     if options.get("debug") == "true":
        print( in_json_str )
@@ -19,32 +19,34 @@ def event_handler_main(in_json_str):
       if p['value'] == "up":
         uplink = p['path'].split(' ')[1]
         response_actions += [
+            # {
+            #     "set-ephemeral-path": {
+            #         "path": f"interface {uplink} ethernet l2cp-transparency tunnel-all-l2cp",
+            #         "value": lldp,
+            #     }
+            # },
+            # {
+            #     "set-ephemeral-path": {
+            #         "path": f"system lldp interface {uplink} admin-state",
+            #         "value": "disable" if lldp else "enable",
+            #     }
+            # }
             {
                 "set-ephemeral-path": {
-                    "path": f"interface {uplink} ethernet l2cp-transparency tunnel-all-l2cp",
-                    "value": lldp,
-                }
-            },
-            {
-                "set-ephemeral-path": {
-                    "path": f"system lldp interface {uplink} admin-state",
-                    "value": "disable" if lldp else "enable",
+                    "path": f"interface {uplink} oper-state",
+                    "value": "down",
                 }
             }
         ]
-        if not lldp:
+        if not reinvoked:
             response_actions += [
-              #{
-              #    "persistent-data": {
-              #     "lldp" : "enabled"
-              #    },
-              # },
               {
                 "reinvoke-with-delay": 60000 # Could make configurable in config
               }
             ]
+            reinvoked = True
 
-    response = {"actions": response_actions}
+    response = {"actions": response_actions, "persistent-data": { "reinvoked": reinvoked } }
     return json.dumps(response)
 
 
