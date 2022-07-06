51bc9fa65ac597eea8b3ea07362623421d6a8486
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-07-06 07:07:45 -0500

    Add interface filter

--- lldp_before_l2cp.py
+++ lldp_before_l2cp.py
@@ -23,6 +23,8 @@ def event_handler_main(in_json_str):
 
     peer_regex = options.get("peer-regex", ".*")
     peer_match = re.compile( peer_regex )
+    peer_if_regex = options.get("peer-interface-regex", ".*")
+    peer_if_match = re.compile( peer_if_regex )
 
     response_actions = []
     peer_map = {}
@@ -76,8 +78,9 @@ def event_handler_main(in_json_str):
     if len(peer_map) > 0 and reinvoked:
       for peer_mac,d in peer_map.items():
         if 'system-name' in d and peer_match.match( d['system-name'] ):
+         if 'port-id' in d and peer_if_match.match( d['port-id'] ):
           if debug:
-            print( f"Found matching LLDP based on regex '{peer_regex}': {d['system-name']}" )
+            print( f"Found matching LLDP based on peer-regex '{peer_regex}' and peer-interface-regex '{peer_if_regex}': {d['system-name']} {d['port-id']}" )
           t = time.gmtime() # in UTC
           timestamp = '{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d} UTC'.format(t[0], t[1], t[2], t[3], t[4], t[5])
           port_id = d[ 'port-id' ] if 'port-id' in d else ""
